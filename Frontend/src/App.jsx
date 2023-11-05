b54b40238fd3d6c0a9657d8b8f6d3d203cffec0d
Hayden Cordeiro <cordeirohayden@gmail.com>
2023-11-05 13:58:23 -0500

    Frontend intial setup + listing of all products

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import CustomCard from './components/CustomCard'
+import './App.css'
+
+
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log("ASDF");
+        const response = await fetch('http://localhost:8080');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        console.log(data)
+        setProducts(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      {products.map((product) => (
+        <CustomCard
+          key={product.id.timestamp}
+          productName={product.productName}
+          productSellingPrice={product.productSellingPrice}
+          productComparisonDetails={product.productComparisonDetails}
+          productThumbnail={product.productThumbnail}
+        />
+      ))}
+    </div>
+  );
+
+}
+
+export default App;
---
+++ Frontend/src/components/CustomCard.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+
+
+function CustomCard({productThumbnail, productName, productSellingPrice, productComparisonDetails}){
+    return (<Card>
+      <CardContent>
+        <img src={productThumbnail} alt="Product Thumbnail" style={{ maxWidth: '100%' }} />
+        <Typography variant="h5" component="div">
+          {productName}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Selling Price: {productSellingPrice}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Comparison Details: {productComparisonDetails}
+        </Typography>
+        <Button variant="contained" fullWidth>
+          View
+        </Button>
+      </CardContent>
+    </Card>);
+}
+
+
+export default CustomCard;
