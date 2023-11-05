42bbf6e3939f27083f71975d3ebdcaa2f33295ad
Hayden Cordeiro <cordeirohayden@gmail.com>
2023-11-05 13:58:00 -0500

    allowed to run on localhost

--- WebScraper/src/main/java/in/co/hayden/WebScraper/Product/ProductController.java
+++ WebScraper/src/main/java/in/co/hayden/WebScraper/Product/ProductController.java
@@ -5,11 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.rmi.ServerException;
 import java.util.List;
@@ -17,6 +13,7 @@ import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173") // Replace with the actual origin of your React app
 public class ProductController {
      @Autowired
      private ProductService productService;
