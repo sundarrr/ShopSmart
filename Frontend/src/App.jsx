74cdd1496d8f160723f8b732a7cc1f6493dd1f6c
Hayden Cordeiro <cordeirohayden@gmail.com>
2023-11-05 16:54:14 -0500

    Added Search Functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,18 +7,21 @@ import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 import CustomCard from './components/CustomCard'
 import './App.css'
+import Autocomplete from '@mui/material/Autocomplete';
 
+import TextField from '@mui/material/TextField';
 
 
 function App() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [searchValue, setSearchValue] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        console.log("ASDF");
         const response = await fetch('http://localhost:8080');
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -27,6 +30,7 @@ function App() {
         const data = await response.json();
         console.log(data)
         setProducts(data);
+        setFilteredProducts(data)
         setLoading(false);
       } catch (error) {
         setError(error);
@@ -37,6 +41,28 @@ function App() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    try {
+      if(searchValue != ""){
+        
+        const filtered = products.filter(product =>
+            product.productName.toLowerCase().includes(searchValue.toLowerCase())
+        );
+        console.log(filtered)
+        setFilteredProducts(filtered);
+      }
+      else{
+        setFilteredProducts(products);
+      }
+    } catch (error) {
+      
+    }
+  }, [searchValue, products]);
+
+  const handleSearchChange = (_, newValue) => {
+    setSearchValue(newValue);
+  };
+
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -46,10 +72,20 @@ function App() {
   }
 
   return (
+    <>
+      <Autocomplete
+        value={searchValue}
+        onChange={handleSearchChange}
+        // options={products.map(product => product.productName)}
+        options={['eggs', 'apples', 'eggplant']}
+        renderInput={(params) => (
+          <TextField {...params} label="Search for a product..." />
+        )}
+      />
     <div  style={{ display:'flex',flexWrap: 'wrap'}}>
-      {products.map((product) => (
+      {filteredProducts.map((product) => (
         <CustomCard
-          key={product.id.timestamp}
+          key={product.id.timestamp + product.id.productName}
           productName={product.productName}
           productSellingPrice={product.productSellingPrice}
           productComparisonDetails={product.productComparisonDetails}
@@ -58,6 +94,7 @@ function App() {
         />
       ))}
     </div>
+    </>
   );
 
 }
